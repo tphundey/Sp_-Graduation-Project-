c45ab00f4987d5c1a578467f26f031f0502ee065
Your Name <105340771+tphundey@users.noreply.github.com>
2024-02-08 20:59:43 +0700

    s2

--- server/app.js
+++ server/app.js
@@ -45,7 +45,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'http://127.0.0.1:5501',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
 }));
